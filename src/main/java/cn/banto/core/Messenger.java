8f08370daaa53dea65691a5f71fed2ddc19b0d6d
banto <780998900@qq.com>
2017-09-20 08:25:18 +0800

    重构项目，优化代码逻辑

--- src/main/java/cn/banto/core/Messenger.java
+++ src/main/java/cn/banto/core/Messenger.java
@@ -1,15 +1,11 @@
 package cn.banto.core;
 
 import cn.banto.exception.SupplicantException;
-import org.apache.log4j.Logger;
 
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.SocketException;
 
 public class Messenger extends  BaseSocket{
-
-    private final Logger logger = Logger.getLogger(Messenger.class);
 
     /**
      * 构造方法
@@ -21,7 +17,6 @@ public class Messenger extends  BaseSocket{
         } catch (SocketException e) {
             throw new SupplicantException("监听3848端口失败,请检查端口是否被占用", e);
         }
-        logger.debug("消息信使已初始化完成");
     }
 
     /**
@@ -34,7 +29,6 @@ public class Messenger extends  BaseSocket{
         } catch (SocketException e) {
             e.printStackTrace();
         }
-        logger.debug("接收消息超时 = "+ receiveTimeout);
     }
 
     /**
@@ -52,7 +46,7 @@ public class Messenger extends  BaseSocket{
      * @return
      * @throws IOException
      */
-    public Message receive(final int appointAction) throws IOException {
+    public Message read(final int appointAction) throws IOException {
         return read(new MessageFilter() {
             @Override
             public boolean after(Message message) {
@@ -61,38 +55,13 @@ public class Messenger extends  BaseSocket{
         });
     }
 
-    /**
-     * 发送并接受消息
-     * @param message 发送的消息
-     * @param appointAction 指定消息动作
-     * @return
-     * @throws IOException
-     */
-    public Message sendAndReceive(Message message, int appointAction) throws IOException {
-        send(message);
-        return receive(appointAction);
-    }
 
     /**
-     * 发送并接受消息
-     * @param message
-     * @param address
-     * @param port
-     * @param appointAction
-     * @return
-     * @throws IOException
+     * 关闭socket
      */
-    public Message sendAndReceive(Message message, InetAddress address, int port, int appointAction) throws IOException {
-        send(message, address, port);
-        return receive(appointAction);
-    }
-
-    /**
-     * 释放资源
-     */
-    public void destroy(){
+    @Override
+    public void stop() {
         socket.close();
         socket = null;
-        logger.debug("已释放消息信使");
     }
 }
